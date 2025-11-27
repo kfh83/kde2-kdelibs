69aae36848b51d79627a27e9971815e9c8ae604f
kfh83 <85626296+kfh83@users.noreply.github.com>
2025-11-27 00:19:20 -0300

    fakes.c

--- kdecore/fakes.c
+++ kdecore/fakes.c
@@ -18,6 +18,8 @@
 
 #include <config.h>
 
+#include <string.h>
+
 #ifndef HAVE_SETENV
 
 #ifdef HAVE_ALLOCA_H
@@ -77,7 +79,7 @@ int unsetenv(const char * name) {
 # include <sys/time.h>
 # include <time.h>
 #else
-# if HAVE_SYS_TIME_H
+# ifdef HAVE_SYS_TIME_H
 #  include <sys/time.h>
 # else
 #  include <time.h>
